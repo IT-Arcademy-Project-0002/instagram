65c0fec9126251862a5c5ca683468f4ec67b508e
mercurios0603 <67107715+mercurios0603@users.noreply.github.com>
2023-12-21 15:41:06 +0900

    test1234

--- src/main/java/com/instargram/instargram/Member/Model/Repository/MemberRepository.java
+++ src/main/java/com/instargram/instargram/Member/Model/Repository/MemberRepository.java
@@ -3,7 +3,6 @@ package com.instargram.instargram.Member.Model.Repository;
 import com.instargram.instargram.Member.Model.Entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
@@ -15,9 +14,4 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Member findByPhoneNumber(String num);
 
     Member findByProviderAndProviderId(String provider, String providerId);
-
-    List<Member> findByUsernameContaining(String keyword);
-    List<Member> findByNicknameContaining(String keyword);
-    List<Member> findByIntroductionContaining(String keyword);
-
 }

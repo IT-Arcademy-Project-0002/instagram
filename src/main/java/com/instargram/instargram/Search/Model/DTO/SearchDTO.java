77952995ffa9cc2eb2261d77a4c92d166566810a
mercurios0603 <67107715+mercurios0603@users.noreply.github.com>
2023-12-28 17:45:35 +0900

    location search proto (#76)

--- src/main/java/com/instargram/instargram/Search/Model/DTO/SearchDTO.java
+++ src/main/java/com/instargram/instargram/Search/Model/DTO/SearchDTO.java
@@ -10,5 +10,9 @@ public class SearchDTO {
    private String username; // 유저 닉네임
    private String profileimage; // 프로필 이미지
    private String introduction; // 상세 정보
+   private String hashtag; // 해시태그
+//   private String locationname; // 장소명
+//   private String locationlat; // 장소위도
+//   private String locationlng; // 장소경도
 
 }

180ad8fdae4bc883e43f7cdff9c70b598b13e3da
mercurios0603 <67107715+mercurios0603@users.noreply.github.com>
2023-12-28 17:44:50 +0900

    location search proto

--- src/main/java/com/instargram/instargram/Explorer/Controller/ExploreController.java
+++ src/main/java/com/instargram/instargram/Explorer/Controller/ExploreController.java
@@ -4,6 +4,8 @@ import com.instargram.instargram.Explorer.Model.DTO.ExploreDTO;
 import com.instargram.instargram.Community.Board.Model.Entity.Board;
 import com.instargram.instargram.Community.Board.Service.BoardService;
 import com.instargram.instargram.Explorer.Service.ExploreService;
+import com.instargram.instargram.Search.Model.DTO.CoordinatesDTO;
+import com.instargram.instargram.Search.Service.SearchService;
 import lombok.Builder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +21,7 @@ public class ExploreController {
 
     private final BoardService boardService;
     private final ExploreService exploreService;
+    private final SearchService searchService;
 
     @GetMapping("")
     public String searchUrl(Model model){
@@ -26,9 +29,21 @@ public class ExploreController {
         List<Board> boardList = this.boardService.getBoard();
         List<ExploreDTO> exploreList = this.exploreService.initExplore(boardList);
 
-        System.out.println(exploreList);
         model.addAttribute("ExploreList", exploreList);
 
         return "Explore/explore";
     }
+    @GetMapping("/location")
+    public String location(Model model) {
+
+        // 향후 ajax를 이용하여 실시간으로 단어를 입력하면
+        // REST API로 검색된 장소의 5~10개 수준을 검색리스트에 포함시키는 로직 필요 (해당 작업은 Search 클래스에서 진행)
+
+        CoordinatesDTO Bykeyword = this.searchService.getCoordinateByKeyword();
+
+        model.addAttribute("ByKeyword", Bykeyword);
+
+        return "Search/location";
+    }
+
 }
---
+++ src/main/java/com/instargram/instargram/Search/Model/DTO/CoordinatesDTO.java
@@ -0,0 +1,15 @@
+package com.instargram.instargram.Search.Model.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CoordinatesDTO {
+    private String x;
+    private String y;
+}

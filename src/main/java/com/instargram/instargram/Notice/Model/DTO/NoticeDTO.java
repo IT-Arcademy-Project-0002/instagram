bee5329fc11dc9b82e89c5500fecb8604aada6bd
mercurios0603 <67107715+mercurios0603@users.noreply.github.com>
2024-01-05 16:09:58 +0900

    Yonggu01 (#107)
    * yonggu 240105 litte develope
    
    * yonggu notice update

--- src/main/java/com/instargram/instargram/Notice/Model/DTO/NoticeDTO.java
+++ src/main/java/com/instargram/instargram/Notice/Model/DTO/NoticeDTO.java
@@ -10,11 +10,34 @@ import java.time.LocalDateTime;
 @Setter
 public class NoticeDTO {
 
+    // 공통 객체
     private Long id;
     private Integer type;
     private Member requestMember;
-    private LocalDateTime createDate;
+    private String createDate;
 
     private boolean follow;
     private boolean follower;
+
+
+    // 게시글 좋아요 : 1
+
+    // 게시글 댓글 : 2
+    private String commentContent; // 댓글 내용
+    private String boardMainImage; // 게시글 메인 이미지
+
+    // 댓글 좋아요 : 3
+
+    // 댓글 대댓글 : 4
+
+    // 디엠 왔을 때 : 5
+
+    // 디엠 좋아요 : 6
+
+    // 스토리 좋아요 : 7
+
+    // 팔로우 요청 : 8
+
+    // 게시글 멤버태그 : 9
+
 }
---
+++ src/main/java/com/instargram/instargram/Notice/Service/NoticeBoardMapService.java
@@ -0,0 +1,8 @@
+package com.instargram.instargram.Notice.Service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class NoticeBoardMapService {
+}

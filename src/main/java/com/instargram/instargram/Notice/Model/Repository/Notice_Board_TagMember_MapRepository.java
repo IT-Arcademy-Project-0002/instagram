d44ec66c2663b20a81a86122690cfa58a8b5a048
mercurios0603 <67107715+mercurios0603@users.noreply.github.com>
2024-01-09 17:47:24 +0900

    notice tagmember develope (#131)

--- src/main/java/com/instargram/instargram/Notice/Model/Repository/Notice_Board_TagMember_MapRepository.java
+++ src/main/java/com/instargram/instargram/Notice/Model/Repository/Notice_Board_TagMember_MapRepository.java
@@ -1,7 +1,11 @@
 package com.instargram.instargram.Notice.Model.Repository;
 
 import com.instargram.instargram.Notice.Model.Entity.Notice_Board_TagMember_Map;
+import com.instargram.instargram.Notice.Model.Entity.Notice_Comment_Like_Map;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface Notice_Board_TagMember_MapRepository extends JpaRepository<Notice_Board_TagMember_Map, Long> {
+
+    Notice_Board_TagMember_Map findByNoticeId(Long noticeId);
+
 }

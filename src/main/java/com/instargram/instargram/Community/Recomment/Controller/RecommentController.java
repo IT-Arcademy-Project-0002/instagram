d341904f1862faaf49f0b007f356306fcb61b939
choyongjae <114542800+deka2904@users.noreply.github.com>
2024-01-17 18:05:40 +0900

    Update RecommentController.java

--- src/main/java/com/instargram/instargram/Community/Recomment/Controller/RecommentController.java
+++ src/main/java/com/instargram/instargram/Community/Recomment/Controller/RecommentController.java
@@ -39,29 +39,6 @@ public class RecommentController {
     private final NoticeService noticeService;
     private final NoticeCommentMapService noticeCommentMapService;
 
-    @PostMapping("/create/{id}")
-    public String create(@PathVariable("id") Long id, RecommentCreateForm recommentCreateForm, BindingResult bindingResult, Principal principal){
-        if (bindingResult.hasErrors()) {
-            return "redirect:/main";
-        }
-        Member member = this.memberService.getMember(principal.getName());
-        Comment comment = this.commentService.getCommentById(id);
-        if (member != null && comment != null) {
-            Recomment recomment = recommentService.create(member, comment, recommentCreateForm.getContent());
-            if (member != comment.getMember() || member != recomment.getMember()) {
-                Notice notice = this.noticeService.createNotice(Enum_Data.COMMENT_RECOMMENT.getNumber(), member, comment.getMember());
-                noticeCommentMapService.createNoticeRecomment(recomment, notice);
-                // 댓글, 대댓글 회원이 아닌 다른 TagMember를 언급할때 실제 회원이 존재한다면(tagMember != null) 알림을 보내는 비즈니스 로직
-                Member tagMember = noticeCommentMapService.createTagMember(recommentCreateForm.getContent());
-                if (member != comment.getMember() && member != recomment.getMember() && tagMember != null) {
-                    Notice noticeForTabMember = this.noticeService.createRecommentTagMemberNotice(Enum_Data.COMMENT_RECOMMENT.getNumber(), member, tagMember);
-                    noticeCommentMapService.createNoticeRecomment(recomment, noticeForTabMember);
-                }
-            }
-        }
-        return "redirect:/main";
-    }
-
 //    @PostMapping("/recomment/create/{id}")
 //    public ResponseEntity<Map<String, Object>> create(@PathVariable("id") Long id, RecommentCreateForm recommentCreateForm,  BindingResult bindingResult, Principal principal){
 //        Map<String, Object> result = new HashMap<>();
